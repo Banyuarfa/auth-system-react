22ecc90f3fa55b4ea0042c5aadd31f10d6b92d2f
Arfa Banyu Santoro <banyuarfa1122@gmail.com>
2025-02-27 10:26:39 +0700

    v0.0.1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/LoginForm-ReactJS/',
+  base: '/react-login-form/',
   plugins: [react()],
 })
